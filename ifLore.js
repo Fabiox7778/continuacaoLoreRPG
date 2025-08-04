46c08e74b304431a0e0fcb6767f163bb99799b83
Fabiox7778 <fabiorabello08@gmail.com>
2025-08-04 10:25:05 -0300

    Prologo

--- ifLore.js
+++ ifLore.js
@@ -1,23 +1,42 @@
 //Atributos do personagem:
-let nome = "Arthur Targaryen";
+const nome = "Arthur Targaryen";
 let classe = "Rei Mago"; //Maior classe possivel
-let nivel = 10000000000000000000000000000000000000000000000;
-let vida = 1000000000000000000000000;
-let moedas = 1000000000;
-let xp = 1000000000000000000000000000000000000000000000000;
+let nivel = 10000000000000000000000000000000000;
+let vida = 100000000000000000000;
+let moedas = 100000;
+let xp = 100000000000000000;
 
+//Novos atributos:
+let manaMaxima = 50000000000;
+let forca = 150000000000000000;
+let defesa = 124000000;
+let agilidade = 12000;
+
+// Estado atual da história:
+let localAtual = "Casa do Rei Demônio";
+let missaoAtual = "Conseguir o Castelo";
 
 //Atributos da Arma:
 const NOMEARMA = "Cumpridor de Promessas"; //Cajado
-let DANO_BASE = 1000000000000000000000000000000000000000000000000000000000;
+let DANO_BASE = 1000000000000000000000000000000000000000;
 const RARIDADEARMA = "Divino";
 const ELEMENTOARMA = "Amaterasu"; //Chama negra que nunca apaga
 
 //Atributos da Armadura:
 const NOMEARMADURA = "Drogon";
-const DEFESA_BASE = 100000000000000000000000000000000000000000000000;
+let DEFESA_BASE = 10000000000000000000000000000000000;
 const RARIDADEARMADURA = "Divino";
 const MATERIAL = "Aço Valiriano";
 
 
-console.log(`Após ${nome} virar ${classe} e ter a melhor arma, o cajado ${NOMEARMA}`)
+//Prólogo:
+
+console.log("📜Prólogo📜");
+console.log("-----------------------------------------");
+console.log(`Anteriormente na história de ${nome}, ele nasceu pobre e foi treinando e treinando mesmo sem magia. em um ataque á sua vila ele conseguiu as armas Divinas de seu mundo`);
+console.log(`${nome} derrotou o Rei Domônio e diminuiu a taxa de ataques a vilas em seu mundo`)
+
+
+//Aventura para conseguir o castelo
+console.log(`Após ${nome} virar ${classe} e ter a melhor arma, o cajado ${NOMEARMA} e melhor armadura ${NOMEARMADURA}. ele busca um bom local para morar.`);
+console.log(`Para isso ele vai precisar de algumas coisas,como muitas moedas e xp.`);
