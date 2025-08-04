3ed07c66e97c840051035dc1cf2f95907e24303a
Fabiox7778 <fabiorabello08@gmail.com>
2025-08-04 09:20:26 -0300

    primeira parte da continuação

---
+++ ifLore.js
@@ -0,0 +1,23 @@
+//Atributos do personagem:
+let nome = "Arthur Targaryen";
+let classe = "Rei Mago"; //Maior classe possivel
+let nivel = 10000000000000000000000000000000000000000000000;
+let vida = 1000000000000000000000000;
+let moedas = 1000000000;
+let xp = 1000000000000000000000000000000000000000000000000;
+
+
+//Atributos da Arma:
+const NOMEARMA = "Cumpridor de Promessas"; //Cajado
+let DANO_BASE = 1000000000000000000000000000000000000000000000000000000000;
+const RARIDADEARMA = "Divino";
+const ELEMENTOARMA = "Amaterasu"; //Chama negra que nunca apaga
+
+//Atributos da Armadura:
+const NOMEARMADURA = "Drogon";
+const DEFESA_BASE = 100000000000000000000000000000000000000000000000;
+const RARIDADEARMADURA = "Divino";
+const MATERIAL = "Aço Valiriano";
+
+
+console.log(`Após ${nome} virar ${classe} e ter a melhor arma, o cajado ${NOMEARMA}`)
